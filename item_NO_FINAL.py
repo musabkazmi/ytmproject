a750f1b16424f72df14da5f734a3c8f026463f42
musabkazmi <musab.kazmi@gmail.com>
2022-01-19 16:00:36 +0500

    third commit

--- item_NO_FINAL.py
+++ item_NO_FINAL.py
@@ -25,11 +25,11 @@ df_final = pd.DataFrame(columns = ["ITEMNO","INV_ITEM_DESCRIPTION","Year","Jan",
 # print(df_main.columns)
 index = 100
 print(df_final)
-# df_final.at[index,'ITEMNO']= 999
-# print(df_final)
-# for i in unique_item_no_tuple:
-#     for j in date_list:
-#         # rslt_df = l2df[l2df['ITEMNO'] == 8177]
-#         df_final.at['ITEMNO']= i
+df_final.at[index,'ITEMNO']= 999
+print(df_final)
+for i in unique_item_no_tuple:
+    for j in date_list:
+        # rslt_df = l2df[l2df['ITEMNO'] == 8177]
+        df_final.at['ITEMNO']= i
 
 
